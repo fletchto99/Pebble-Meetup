94dc25c52132de04261d5701beb033b105ce81c6
Matthew Langlois <fletchto99@gmail.com>
2015-05-22 23:13:14 -0400

    Refactor SQL to database folder Added Version table creation script Insertion scripts for base versions

---
+++ database/Update_Version.sql
@@ -0,0 +1,5 @@
+INSERT INTO Versions (Version_Code, Version_Description, Version_Changes, Pre_Release)
+VALUES ('2.06', 'This is a minor app which allows users to check upcoming events in their area. Currently it supports searching for Pebble Meetups and Pebble Groups.', 'Test about version', 0)
+
+INSERT INTO Versions (Version_Code, Version_Description, Version_Changes, Pre_Release)
+VALUES ('2.07-ALPHA', 'This is a minor app which allows users to check upcoming events in their area. Currently it supports searching for Pebble Meetups and Pebble Groups.', 'Support for About screen and version notifications.', 1)
