ae7d64495c099391030a63a54f21805b3c54a297
Matthew Langlois <fletchto99@gmail.com>
2015-05-25 20:40:36 -0400

    Use the latest version of PHPebbleTimeline

--- web/functions/TimelineAPI/PinAction.php
+++ web/functions/TimelineAPI/PinAction.php
@@ -1,7 +1,8 @@
 <?php
 
-
 namespace TimelineAPI;
+
+use Exception;
 
 class PinAction {
 
@@ -9,18 +10,16 @@ class PinAction {
     private $launchcode;
     private $type;
 
-    function __construct($title, $launchcode, $type= null) {
+    function __construct($title, $launchcode, PinActionType $type) {
         if ($title == null || $launchcode == null) {
-            throwException("Launchcode and title both must be specified");
+            throw new Exception("Launchcode and title both must be specified");
         }
         $this -> title = $title;
         $this -> launchcode = $launchcode;
-        if ($type == null) {
-            $this->type = PinActionType::OPEN_WATCH_APP;
-        }
+        $this -> type = $type;
     }
 
-    function getDataArray() {
+    function getData() {
         return array_filter([ 'type' => $this -> type, 'title' => $this -> title, 'launchcode' => $this -> launchcode]);
     }
 
